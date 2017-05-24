9ea10ee5ea6ab82ddab0d4c83b5da50fca63c68d
Denis Zadniprovskiy <dzadnip@gmail.com>
2017-05-24 16:17:09 -0700

    Initial commit
    Created a new Console project to test code for HackerRank challanges.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hackerrank_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# hackerrank-challenges
-Place to save my test code while working on HackerRank challenges
