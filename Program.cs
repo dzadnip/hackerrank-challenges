6ca6ba68901e161de8f4f287bae15ef3280a2173
Denis Zadniprovskiy <dzadnip@gmail.com>
2017-05-24 17:13:46 -0700

    Arrays - DS
    Print all integers in reverse order as a single line of space-separated integers.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace hackerrank_challenges
 {
@@ -6,7 +9,23 @@ namespace hackerrank_challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numberOfIntegers = Console.ReadLine();
+
+			int[] arr = Console.ReadLine()
+                               .Split(' ')
+                               .Select(n => Convert.ToInt32(n))
+                               .ToArray();
+            string output = "";
+
+            for (int i = 1; i <= arr.Length; i++) {
+                output = output + arr[arr.Length - i].ToString();
+                if (i != arr.Length)
+                {
+                    output = output + " ";
+                }
+            }
+
+            Console.WriteLine(output);
         }
     }
 }
