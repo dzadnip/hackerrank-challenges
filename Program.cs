d4dc824fc6e84caf7f9ada9f9afb35d16237c253
Denis Zadniprovskiy <dzadnip@gmail.com>
2017-12-16 18:18:40 -0800

    A Very Big Sum

--- Program.cs
+++ Program.cs
@@ -5,59 +5,24 @@ using System.Linq;
 class Solution
 {
 
-    static int[] Solve(int a0, int a1, int a2, int b0, int b1, int b2)
+    static long aVeryBigSum(int n, long[] ar)
     {
         // Complete this function
-        int aPoints = 0;
-        int bPoints = 0;
-
-        if (a0 > b0)
+        long sum = 0;
+        for (int i = 0; i < n; i++)
         {
-            aPoints++;
+            sum = sum + ar[i];
         }
-        else if (b0 > a0)
-        {
-            bPoints++;
-        }
-
-        if (a1 > b1)
-        {
-            aPoints++;
-        }
-        else if (b1 > a1)
-        {
-            bPoints++;
-        }
-
-        if (a2 > b2)
-        {
-            aPoints++;
-        }
-        else if (b2 > a2)
-        {
-            bPoints++;
-        }
-
-        int[] results = new int[] { aPoints, bPoints };
-
-        return results;
+        return sum;
     }
 
     static void Main(String[] args)
     {
-        string[] tokens_a0 = Console.ReadLine().Split(' ');
-        int a0 = Convert.ToInt32(tokens_a0[0]);
-        int a1 = Convert.ToInt32(tokens_a0[1]);
-        int a2 = Convert.ToInt32(tokens_a0[2]);
-        string[] tokens_b0 = Console.ReadLine().Split(' ');
-        int b0 = Convert.ToInt32(tokens_b0[0]);
-        int b1 = Convert.ToInt32(tokens_b0[1]);
-        int b2 = Convert.ToInt32(tokens_b0[2]);
-        int[] result = Solve(a0, a1, a2, b0, b1, b2);
-        Console.WriteLine(String.Join(" ", result));
-
-
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
+        long result = aVeryBigSum(n, ar);
+        Console.WriteLine(result);
     }
 }
-
 
