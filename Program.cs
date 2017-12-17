c8a72e5a5338096c20046aaff91cd9ba1d854d64
Denis Zadniprovskiy <dzadnip@gmail.com>
2017-12-16 20:05:42 -0800

    Let's try this one more time.

--- Program.cs
+++ Program.cs
@@ -13,10 +13,13 @@ namespace hack
             for (int i = 0; i < n; i++)
             {
                 sumOfDiagnal1 = sumOfDiagnal1 + a[i][i];
-                sumOfDiagnal2 = sumOfDiagnal2 + a[i][n - i - 1];
             }
-
-            int absoluteDifference = Math.Abs(sumOfDiagnal1 - sumOfDiagnal2);
+            for (int i = n - 1; i > 0; i--)
+            {
+                sumOfDiagnal2 = sumOfDiagnal2 + a[i][i];
+            }
+            Console.WriteLine(sumOfDiagnal1);
+            Console.WriteLine(sumOfDiagnal2);
         }
 
         public static void Main(string[] args)
@@ -25,7 +28,8 @@ namespace hack
             int[][] a = new int[n][];
             for (int a_i = 0; a_i < n; a_i++)
             {
-                a[a_i] = Console.ReadLine().Split(' ').Select(s => Int32.Parse(s)).ToArray();
+                string[] a_temp = Console.ReadLine().Split(' ');
+                a[a_i] = a_temp.Select(s => Int32.Parse(s)).ToArray();
             }
             DiagonalDifference(n, a);
         }
