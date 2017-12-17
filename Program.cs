cad3f37c96e35258d7c6d45eca1566bac78be813
Denis Zadniprovskiy <dzadnip@live.com>
2017-12-16 20:09:52 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace hack
@@ -13,13 +13,10 @@ namespace hack
             for (int i = 0; i < n; i++)
             {
                 sumOfDiagnal1 = sumOfDiagnal1 + a[i][i];
+                sumOfDiagnal2 = sumOfDiagnal2 + a[i][n - i - 1];
             }
-            for (int i = n - 1; i > 0; i--)
-            {
-                sumOfDiagnal2 = sumOfDiagnal2 + a[i][i];
-            }
-            Console.WriteLine(sumOfDiagnal1);
-            Console.WriteLine(sumOfDiagnal2);
+
+            int absoluteDifference = Math.Abs(sumOfDiagnal1 - sumOfDiagnal2);
         }
 
         public static void Main(string[] args)
@@ -28,8 +25,7 @@ namespace hack
             int[][] a = new int[n][];
             for (int a_i = 0; a_i < n; a_i++)
             {
-                string[] a_temp = Console.ReadLine().Split(' ');
-                a[a_i] = a_temp.Select(s => Int32.Parse(s)).ToArray();
+                a[a_i] = Console.ReadLine().Split(' ').Select(s => Int32.Parse(s)).ToArray();
             }
             DiagonalDifference(n, a);
         }
